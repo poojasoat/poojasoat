7235e5787b8f5401a87ae37f4dc0e613ff37fc71
poojasoat@gmail.com <poojasoat@gmail.com>
2020-03-10 17:29:41 +0530

    Add three files

---
+++ footer.php
@@ -0,0 +1,54 @@
+<!-- footer -->
+<footer style="    padding-bottom: 15%;">
+        <div class="container-fluid" style="padding-left: 7%;padding-right: 7%;">
+          <div class="row">
+            
+            <div class="col-xl-4 col-lg-3 col-md-6 col-sm-6">
+              <div class="part1">
+                <p style="font-weight: 600;">Thank you for visiting us</p>
+                 <p>Please feel free to contact<br>us with any questions you <br>may have ,as we are happy <br>to assit you.</p>
+              </div>
+            </div>
+            <div class="col-xl-3 col-lg-4 col-md-6 col-sm-6">
+             
+            </div>
+            <div class="col-xl-2 col-lg-2 col-md-6 col-sm-6">
+              
+            </div>
+            <div class="col-xl-3 col-lg-3 col-md-6 col-sm-6 text-center">
+              <div class="part4">
+                
+                <ul class="social-media1">
+                   
+                  <li><a href="https://www.google.com/search?client=firefox-b-1-d&ei=MFXcXbffCYru-gTN4YbIBg&q=Priddy+Services+yuba+city+california+&oq=Priddy+Services+yuba+city+california+&gs_l=psy-ab.3..33i299.6362.10696..10793...3.2..1.264.3424.0j23j1......0....1..gws-wiz.......0i71j0i22i30j33i160.Ec8hzHUt8OE&ved=0ahUKEwi3t_iWtIbmAhUKt54KHc2wAWkQ4dUDCAo&uact=5" target="_blank">
+                    <img class="img-fluid pt-2" src="images/google.png" alt="facebook"></a></li>
+                     <li><a href="https://www.facebook.com/priddyservices/" target="_blank">
+                    <img class="img-fluid" src="images/fb.png" alt="instagram"></a></li>
+                       <li>
+                     <li><a href="" target="_blank">
+                    <img class="img-fluid" src="images/yl.png" alt="instagram"></a></li>
+
+                
+               
+                  
+                </ul>
+                
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+    
+      <!-- footer -->
+
+<script type="text/javascript">
+    $(function () {
+    var url = window.location.pathname,
+    urlRegExp = new RegExp(url.replace(/\/$/, '') + "$");
+    $('a').each(function () {
+    if (urlRegExp.test(this.href.replace(/\/$/, ''))) {
+    $(this).addClass('active');
+    }
+    });
+    });
+</script>
