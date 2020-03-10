53a36b0e777b8b2de872202c5e9b7ded4347cfd2
poojasoat@gmail.com <poojasoat@gmail.com>
2020-03-10 17:28:02 +0530

    Add three files

---
+++ header.php
@@ -0,0 +1,59 @@
+<header>
+        <div class="container-fluid top-bar-fluid">
+          <div class="top-bar">
+            <div class="row" style="border-bottom: 1px solid black;">
+              <div class="col-md-6">
+                 <a class="navbar-brand" href="#">
+                  
+              
+                 <span class="website-heading">Priddy Services</span></a>
+              </div>
+              <div class="col-md-6 contact-right">
+                <div class="contact-main">
+                   <div class="container px-0 pt-2">
+      <div class="d-flex justify-content-end">
+        <div class="searchbar">
+          <input class="search_input" type="text" name="" placeholder="Search Here...">
+          <a href="#" class="search_icon"><i class="fas fa-search"></i></a>
+        </div>
+      </div>
+    </div>
+                  <p class="contact-nav-link text-right pt-3">
+                    <a href="tel:1 530-301-0008" data-content="1 530-301-0008" data-type="phone">+1 530-301-0008</a></p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <nav class="navbar navbar-expand-lg navbar-light bg-color top-bar-fluid py-0">
+          <div class="container-fluid px-0">
+ 
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse justify-content-start" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link color-menu" href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link color-menu" href="index.php#about">About</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link color-menu" href="index.php#services">Service</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link color-menu" href="index.php#test">Testimonials</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link color-menu" href="index.php#contact">Contact</a>
+      </li>
+     </ul>
+  
+  </div>
+</div>
+</nav>
+</header>
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
